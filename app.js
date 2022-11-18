16101162a0cf0b2d6071f26d1f3cacc6b040c910
Rachit Saxena <72144798+horcruxxxx@users.noreply.github.com>
2022-11-19 00:06:44 +0530

    Added Level 3 Security(hashing)
    Level 3 --> Hashing

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-require('dotenv').config();         //we configure this so that it can access the enviournment variables.
-const express = require("express");
+require('dotenv').config();                           //we configure this so that it can access the enviournment variables.
+const express    = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-const encrypt  = require("mongoose-encryption");    //we use this package for encryption and authentication.
+const ejs        = require("ejs");
+const mongoose   = require("mongoose");
+// const encrypt    = require("mongoose-encryption");    //we use this package for encryption and authentication.
+const md5        = require("md5");
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-// const userSchema = {    //Creating schema //This is simple javascript Object.
-//     email:String,       //We can use this schema unless untill we are not doing anything fancy/Complex with this.
-//     password:String     //Here we have to add plugins to our Schema thats why we have to use mongoose.schemas!
+// const userSchema = {                      //Creating schema //This is simple javascript Object.
+//     email:String,                         //We can use this schema unless untill we are not doing anything fancy/Complex with this.
+//     password:String                       //Here we have to add plugins to our Schema thats why we have to use mongoose.schemas!
 // };
 
-const userSchema = new mongoose.Schema({    //This is no longer a Simple java script obkect ,it is a object which was created using   
-    email:String,                           //mongoose.schema class 
+const userSchema = new mongoose.Schema({     //This is no longer a Simple java script obkect ,it is a object which was created using   
+    email:String,                            //mongoose.schema class 
     password:String
 });
 
@@ -22,7 +23,7 @@ const userSchema = new mongoose.Schema({    //This is no longer a Simple java sc
 const secretcode = process.env.SECRETCODE;      //after using environment variables
 
 
-userSchema.plugin(encrypt,{secret:secretcode, encryptedFields : ['password']}); 
+// userSchema.plugin(encrypt,{secret:secretcode, encryptedFields : ['password']}); 
 //This will encrypt our entire databse and then how we going to search for emails?. thats why we use encryption field.
 
 const User = new mongoose.model("User",userSchema);     //creating Model
@@ -50,7 +51,8 @@ app.post("/register",function(req,res){
 
     newUser = new User({
         email : req.body.username,
-        password  : req.body.password
+        // password  : req.body.password    //instead os saving the password as a string we are going to use md5 hash.
+        password : md5(req.body.password)
     });
     
     newUser.save(function(err){
@@ -66,7 +68,7 @@ app.post("/login",function(req,res){
         if(err) connsole.log(err);
         else{
             if(founduser){
-                if(founduser.password === password){
+                if(founduser.password === md5(password)){
                     res.render("secrets");
                 }
             }
